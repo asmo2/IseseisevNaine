5c1e179704db949562237c732878e03d603dbb0e
asmo2 <asmo.voitka@vikk.ee>
2022-05-30 17:27:31 +0300

    Yas

--- Iseseisvad tööd/Rally/main.py
+++ Iseseisvad tööd/Rally/main.py
@@ -10,6 +10,8 @@ screen = pygame.display.set_mode((screenX, screenY))
 
 clock = pygame.time.Clock()
 
+Score = 0
+
 blue = [0, 45, 167]
 f1_blue_image = pygame.image.load("img/f1_blue.png")
 f1_red_image = pygame.image.load("img/f1_red.png")
@@ -22,13 +24,15 @@ f1_blue_posX = 300
 
 f1_red_speed = 8
 f1_red_posY = 230
-f1_red_posX = 300
+f1_red_posX = 420
 
 bg_rally_speed = 5
 bg_rally_posY = 0
 
 bg_rally2_posY = -480
-while True:
+
+gameover = False
+while not gameover:
     clock.tick(60)
     events = pygame.event.get()
     for i in events:
@@ -41,7 +45,6 @@ while True:
             if i.key == pygame.K_RIGHT and f1_blue_posX != 420:
                 f1_blue_posX += 120
 
-
     f1_red_posY += f1_red_speed  # lahutad positsioonist kiiruse
     bg_rally_posY += bg_rally_speed  # liidab positsioonile kiiruse
     bg_rally2_posY += bg_rally_speed  # liidab positsioonile kiiruse
@@ -49,7 +52,7 @@ while True:
     screen.blit(bg_rally2, (0, bg_rally2_posY))
     screen.blit(bg_rally, (0, bg_rally_posY))
 
-    f1_blue = pygame.Rect(f1_blue_posX,f1_blue_posY, 45, 90 )
+    f1_blue = pygame.Rect(f1_blue_posX, f1_blue_posY, 45, 90)
     screen.blit(f1_blue_image, f1_blue)
 
     f1_red = pygame.Rect(f1_red_posX, f1_red_posY, 45, 90)
@@ -66,5 +69,18 @@ while True:
     if f1_red_posY > 480:
         f1_red_posY = -90
         f1_red_speed = random.randrange(6, 15, 1)
+        f1_red_posX = random.randrange(180, 540, 120)
+
+    if f1_red.colliderect(f1_blue):
+        gameover = True
     # screen.fill(blue)
     pygame.display.flip()
+
+while gameover:
+    events = pygame.event.get()
+    for i in events:
+        if i.type == pygame.QUIT:
+            quit()
+    screen.fill(blue)
+    pygame.display.flip()
+
