69edd9fbff8305b432ade5c6af0935577ac0bba9
asmo2 <asmo.voitka@vikk.ee>
2022-05-30 17:12:22 +0300

    Yas

--- Iseseisvad tööd/Rally/main.py
+++ Iseseisvad tööd/Rally/main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 pygame.init()
 
@@ -10,16 +11,18 @@ screen = pygame.display.set_mode((screenX, screenY))
 clock = pygame.time.Clock()
 
 blue = [0, 45, 167]
-f1_blue = pygame.image.load("img/f1_blue.png")
-f1_red = pygame.image.load("img/f1_red.png")
+f1_blue_image = pygame.image.load("img/f1_blue.png")
+f1_red_image = pygame.image.load("img/f1_red.png")
+f1_red_image = pygame.transform.rotate(f1_red_image, 180)
 bg_rally = pygame.image.load("img/bg_rally.jpg")
 bg_rally2 = pygame.image.load("img/bg_rally.jpg")
 
-f1_blue_speed = 3
 f1_blue_posY = 344
+f1_blue_posX = 300
 
-f1_red_speed = 3
+f1_red_speed = 8
 f1_red_posY = 230
+f1_red_posX = 300
 
 bg_rally_speed = 5
 bg_rally_posY = 0
@@ -31,15 +34,26 @@ while True:
     for i in events:
         if i.type == pygame.QUIT:
             quit()
-    f1_red_posY -= f1_red_speed  # lahutad positsioonist kiiruse
-    f1_blue_posY -= f1_blue_speed  # lahutad positsioonist kiiruse
+        if i.type == pygame.KEYDOWN:
+            if i.key == pygame.K_LEFT and f1_blue_posX != 180:
+                f1_blue_posX -= 120
+
+            if i.key == pygame.K_RIGHT and f1_blue_posX != 420:
+                f1_blue_posX += 120
+
+
+    f1_red_posY += f1_red_speed  # lahutad positsioonist kiiruse
     bg_rally_posY += bg_rally_speed  # liidab positsioonile kiiruse
     bg_rally2_posY += bg_rally_speed  # liidab positsioonile kiiruse
 
     screen.blit(bg_rally2, (0, bg_rally2_posY))
     screen.blit(bg_rally, (0, bg_rally_posY))
-    screen.blit(f1_blue, (300, f1_blue_posY))
-    screen.blit(f1_red, (200, f1_red_posY))
+
+    f1_blue = pygame.Rect(f1_blue_posX,f1_blue_posY, 45, 90 )
+    screen.blit(f1_blue_image, f1_blue)
+
+    f1_red = pygame.Rect(f1_red_posX, f1_red_posY, 45, 90)
+    screen.blit(f1_red_image, f1_red)
 
     if bg_rally_posY > screenY:
         bg_rally_posY = -480
@@ -48,8 +62,9 @@ while True:
 
     if f1_blue_posY < 0 - 90:
         f1_blue_posY = 480
-    if f1_red_posY < 0 - 90:
-        f1_red_posY = 480
 
+    if f1_red_posY > 480:
+        f1_red_posY = -90
+        f1_red_speed = random.randrange(6, 15, 1)
     # screen.fill(blue)
     pygame.display.flip()
