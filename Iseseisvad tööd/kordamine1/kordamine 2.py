b71682a4511ffc3b7b4fbd45d4c38a6f11027be9
asmo2 <asmo.voitka@vikk.ee>
2022-06-07 10:22:24 +0300

    azds

--- Iseseisvad tööd/kordamine1/kordamine 2.py
+++ Iseseisvad tööd/kordamine1/kordamine 2.py
@@ -2,7 +2,18 @@ import socket
 import sys
 import time
 
-s = socket.socket()
-host = input(str("Please enter the hostname of the server :"))
-s.connect((host.port))
-print("connecter to chat server")
+s = socket.socket() #loob uue pistiku
+host = input(str("Please enter the hostname of the server :")) #võtab kasutaja inputi
+port = 8080 #port on 8080
+s.connect((host,port)) #ühendab pistikupesa aadressile
+print("connected to chat server") #prindib et connected to chat
+while 1: #while loop
+    incoming_message = s.recv(1024) #võtab infot
+    incoming_message = incoming_message.decode()
+    print("Server: ", incoming_message) #prindib "server:" ja serverist kirjutatud sõnumi
+    print("") #prindib tühiku
+    message = input(str(">> ")) #prindib nooled ja võtab kasutaja sisestatud sõnumi
+    message = message.encode() #kodeerib sõnumi
+    s.send(message) #
+    print("message has been sent...") #prindib teksti sulgude sees
+    print("") #prindib tühiku
--- Iseseisvad tööd/kordamine1/kordamine.py
+++ Iseseisvad tööd/kordamine1/kordamine.py
@@ -2,17 +2,27 @@ import socket
 import sys
 import time
 
-s = socket.socket()
-host = socket.gethostname()
-print("Server will start on host: ", host)
-port = 8080
-s.bind((host.port))
-print("")
-print("Server done binding to host and port successfully")
-print("")
-print("Server is waiting for incoming connection")
-print("")
+s = socket.socket() #loob uue pistikupesa
+host = socket.gethostname() #loob uue pistikupesa
+print("Server will start on host: ", host) #prindib
+port = 8080 #port 8080
+s.bind((host, port)) #
+print("") #prindib tühiku
+print("Server done binding to host and port successfully") #prindib et serveril sai cliendiga ühendamine valmis
+print("") #prindib tühiku
+print("Server is waiting for incoming connection")#prindib et server ootab sissetulevad connectioni
+print("") #prindib tühiku
 s.listen(1)
-conn, addr = s.accept()
-print(addr, "has connected to the server and is now onlline ...")
-print("")
+conn, addr = s.accept()#seab muutujad ühenduse loomiseks
+print(addr, "has connected to the server and is now onlline ...") #prindib et server on nüüd online
+print("") #prindib tühiku
+while 1: #while loop
+    message = input(str(">>")) #prindib nooled
+    message = message.encode() #kodeerib sõnumi
+    conn.send(message) #saadab sõnumi
+    print("message has been sent...") #prindib et sõnum saadeti välja
+    print("") #prindib tühiku
+    incoming_message = conn.recv(1024) #võtab infot
+    incoming_message = incoming_message.decode() #dekodeerib sõnumi
+    print("Client: ", incoming_message) #prindib client ja sissetulnud sõnumi
+    print("") #prindib tühiku
